9f95aa797c2360479a99ad1dd6c5d367503195dd
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-09-16 16:18:25 +0200

    pokedex

---
+++ src/components/Pokemons/Pokedex.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import "./Style.css";
+const Pokedex = () => {
+  return <div className="container">Pokedex</div>;
+};
+
+export default Pokedex;
