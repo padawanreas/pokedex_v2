3d9b45a2d4e484769996a6a5385cef438ef35e76
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-09-16 16:17:28 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Nav from "./components/Nav";
+import Nav from "./components/Nav/Nav";
+import Pokedex from "./components/Pokemons/Pokedex";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Pokedex />
     </div>
   );
 }
